6691d54acd38e01c276058e51b4a64dba1a9806e
Santigil0814 <santigil0814@gmail.com>
2022-04-18 16:32:23 -0500

    cambios

--- Index.php
+++ Index.php
@@ -1,22 +1,23 @@
 <?php
 
-// $curl = curl_init();
+$curl = curl_init();
 
-// curl_setopt_array($curl, array(
-//   CURLOPT_URL => 'https://rickandmortyapi.com/api/episode',
-//   CURLOPT_RETURNTRANSFER => true,
-//   CURLOPT_ENCODING => '',
-//   CURLOPT_MAXREDIRS => 10,
-//   CURLOPT_TIMEOUT => 0,
-//   CURLOPT_FOLLOWLOCATION => true,
-//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//   CURLOPT_CUSTOMREQUEST => 'GET',
-// ));
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://rickandmortyapi.com/api/episode',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
 
-// $response = curl_exec($curl);
+$response = curl_exec($curl);
 
-// curl_close($curl);
-// echo $response;
+curl_close($curl);
+
+$api = json_decode($response,true);
 
 ?>
 
@@ -27,40 +28,52 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rick And Morty</title>
-  <link rel="stylesheet" href="index.css" type="text/css" media="all" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="index.css" type="text/css" media="all" />
+  <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROVGAz_bMma7Xc6od4CvojOmucnXvQyVrg5Q&usqp=CAU">
 </head>
 <body>
-  <h1>hola</h1>
-  <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROVGAz_bMma7Xc6od4CvojOmucnXvQyVrg5Q&usqp=CAU" alt="" width="30" height="30" class="d-inline-block align-text-top">
+      Rick and Morty API
+    </a>
+  </div>
+</nav>
+
+<form class="cuerpo">
+
+  <h1>Rick and Morty</h1>
+ 
+  <div class="cards">
+
+ <?php
+    foreach ($api['results'] as $key => $value) {
+      $name = $value['name'];
+      $episode = $value['episode'];
+      $air_date = $value['air_date'];
+  ?>
+ 
+  
+    <div class="card">
+      <img class="imagen" src="https://es.web.img3.acsta.net/pictures/18/10/31/17/34/2348073.jpg">
+      <div class="card-body">
+        <h4 class="card-title"><?php echo $name ?></h4>
+        <h6 class="card-text"><?php echo $episode ?></h6>
+        <h6 class="card-text"><?php echo $air_date ?></h6>
+        <div class="btn">
+          <a href="#">DETALLE</a>
+        </div>
+      </div>
+    </div>
+  
+  <?php
+    }
+  ?>
+
+  </form>
+
 </body>
 </html>
